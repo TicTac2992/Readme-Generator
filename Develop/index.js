634aa2205032d4a8b1f9c4eb886f3af691f40cb7
Jared Varnum <jvarnum3@gmail.com>
2020-07-12 00:22:04 -0500

    index.js work in progress, modified package.json, consolidated

--- Develop/index.js
+++ Develop/index.js
@@ -1,32 +1,76 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
 
-// function to write README file
-function writeToFile(fileName, data) {
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What is the title of your project?"
+    },
+    {
+      type: "input",
+      name: "Description",
+      message: "Can you describe your project?"
+    },
+    {
+      type: "input",
+      name: "tableOfContents",
+      message: "What are the contents of your README?"
+    },
+    {
+      type: "input",
+      name: "food",
+      message: "What is your favorite food?"
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username"
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "Enter your LinkedIn URL."
+    }
+  ]);
 }
 
-// function to initialize program
-function init() {
+async function init() {
+  console.log("hi")
+  try {
+    const answers = await promptUser();
+
+    const html = generateHTML(answers);
+
+    await writeFileAsync("index.html", html);
 
 }
 
-// function call to initialize program
-init();
 
-// GIVEN a command-line application that accepts user input
 // WHEN I am prompted for information about my application repository
-// THEN a quality, professional README.md is generated with the title of your project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
+// THEN a quality, professional README.md is generated with the title of your project and sections entitled 
+
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+
+
 // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
 // WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added hear the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// THEN a badge for that license is added hear the top of the README and a notice is added to the section of the README entitled 
+//     License that explains which license the application is covered under
+
 // WHEN I enter my GitHub username
 // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
 // WHEN I enter my email address
 // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
